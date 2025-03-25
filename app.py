423f907100c67623c87a87d38508f3d41455f2db
hiapplyco <155493890+hiapplyco@users.noreply.github.com>
2025-03-25 07:34:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from phi.agent import Agent
 from phi.model.google import Gemini
 from phi.tools.duckduckgo import DuckDuckGo
-from phi.tools.youtube_tools import YouTubeTools
 
 import google.generativeai as genai
 from google.generativeai import upload_file, get_file
